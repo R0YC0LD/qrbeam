f95b1ad92c526d8ea96a62b2e896c330fbeaab0b
R0YC0LD <77665809+R0YC0LD@users.noreply.github.com>
2026-08-01 11:16:19 +0300

    Upgrade optical speed and loss recovery

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'qrbeam-v1';
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './manifest.webmanifest', './icon.svg']))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+const CACHE = 'qrbeam-v2';
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './manifest.webmanifest', './icon.svg'])).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
   if (event.request.method === 'GET' && response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
   return response;
